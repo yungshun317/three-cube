5e4f321f48499024b984d4c988be182cf4e4327d
yungshun317 <seraph317@gmail.com>
2023-03-01 10:22:06 +0800

    [FEAT] Create and render a cube based on `BoxGeometry`

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 import * as THREE from '/three.module.min.js';
 
-// Scene
+// [1] Scene
 const scene = new THREE.Scene();
+
+// [2] Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "purple" });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// [3] Camera
+const aspect = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+const camera = new THREE.PerspectiveCamera(
+    75,
+    aspect.width / aspect.height,
+    1,
+    2000
+);
+camera.position.z = 3;
+scene.add(camera);
+
+// [4] Renderer
+// Select the `Canvas` element
+const canvas = document.querySelector(".draw");
+// Add the `WebGLRenderer`
+const renderer = new THREE.WebGLRenderer({ canvas });
+// Renderer size
+renderer.setSize(aspect.width, aspect.height);
+// Display what the camera in the scene captured
+renderer.render(scene, camera);
