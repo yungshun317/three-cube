08dda36a9a170e700403e6625bb7d7c1666ca5e3
yungshun317 <seraph317@gmail.com>
2023-03-02 10:44:38 +0800

    [FEAT] Add two cubes to a group

--- script.js
+++ script.js
@@ -3,12 +3,30 @@ import * as THREE from '/three.module.min.js';
 // [1] Scene
 const scene = new THREE.Scene();
 
-// [2] Mesh
+// [2] Group
+const group = new THREE.Group();
+
+// Mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "purple" });
 const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.x = 1;
+// mesh.position.y = 1;
+// mesh.scale.x = 2;
+// mesh.scale.y = 3;
+// mesh.rotation.x = Math.PI * 0.25;
+mesh.position.z = 1;
 
-scene.add(mesh);
+// scene.add(mesh);
+
+const geometryT = new THREE.BoxGeometry(1, 1, 1);
+const materialT = new THREE.MeshBasicMaterial({ color: "green" });
+const meshT = new THREE.Mesh(geometryT, materialT);
+meshT.position.y = 2;
+
+group.add(mesh, meshT);
+group.position.x = 3;
+scene.add(group);
 
 // [3] Camera
 const aspect = {
@@ -21,7 +39,9 @@ const camera = new THREE.PerspectiveCamera(
     1,
     2000
 );
-camera.position.z = 3;
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 5;
 scene.add(camera);
 
 // [4] Renderer
