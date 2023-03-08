08bd97abcce74b37ebe0896c7187dd90707829c5
yungshun317 <seraph317@gmail.com>
2023-03-08 13:24:45 +0800

    [FEAT] Use `OrthographicCamera`

--- script.js
+++ script.js
@@ -3,32 +3,7 @@ import * as THREE from '/three.module.min.js';
 // [1] Scene
 const scene = new THREE.Scene();
 
-// [2] Object
-
-// Mesh
-const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-const material = new THREE.MeshBasicMaterial({ color: "purple" });
-const mesh = new THREE.Mesh(geometry, material);
-
-scene.add(mesh);
-
-// [3] Camera
-const aspect = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height,);
-camera.position.z = 3;
-scene.add(camera);
-
-// [4] Renderer
-// Select the `Canvas` element
-const canvas = document.querySelector(".draw");
-// Add the `WebGLRenderer`
-const renderer = new THREE.WebGLRenderer({ canvas });
-// Renderer size
-renderer.setSize(aspect.width, aspect.height);
-
+// Responsive
 window.addEventListener("resize", () => {
     // New size
     aspect.width = window.innerWidth;
@@ -42,6 +17,36 @@ window.addEventListener("resize", () => {
     renderer.setSize(aspect.width, aspect.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
+
+// [2] Object
+
+// Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "purple" });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// [3] Camera
+const aspect = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000); //
+// const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height,);
+camera.position.x = 3;
+camera.position.y = 3;
+camera.position.z = 3;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+// [4] Renderer
+// Select the `Canvas` element
+const canvas = document.querySelector(".draw");
+// Add the `WebGLRenderer`
+const renderer = new THREE.WebGLRenderer({ canvas });
+// Renderer size
+renderer.setSize(aspect.width, aspect.height);
 
 // `Clock` class
 const clock = new THREE.Clock();
