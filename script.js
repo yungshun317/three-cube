ce11c43322665a719f4967b4d4a87ec5b2ca6441
yungshun317 <seraph317@gmail.com>
2023-03-21 12:47:37 +0800

    [FEAT] Add `HemisphereLight` with color fading from the sky color to the ground color

--- script.js
+++ script.js
@@ -19,12 +19,19 @@ const materialColor = {
 // gui.add(ambientLight, "intensity", 0, 1, 0.01);
 
 // DirectionalLight
-const directionalLight = new THREE.DirectionalLight("#ffffff", 0.5);
-directionalLight.position.set(0, 5, 5);
-scene.add(directionalLight);
-gui.add(directionalLight, "intensity", 0, 1, 0.01);
-gui.add(directionalLight.position, "x", -3, 3, 0.01);
-gui.add(directionalLight.position, "y", -3, 3, 0.01);
+// const directionalLight = new THREE.DirectionalLight("#ffffff", 0.5);
+// directionalLight.position.set(0, 5, 5);
+// scene.add(directionalLight);
+// gui.add(directionalLight, "intensity", 0, 1, 0.01);
+// gui.add(directionalLight.position, "x", -3, 3, 0.01);
+// gui.add(directionalLight.position, "y", -3, 3, 0.01);
+
+// HemisphereLight
+const hemisphereLight = new THREE.HemisphereLight("blue", "yellow", 1);
+scene.add(hemisphereLight);
+
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight);
+scene.add(hemisphereLightHelper);
 
 // Responsive
 window.addEventListener("resize", () => {
@@ -47,8 +54,11 @@ window.addEventListener("resize", () => {
 const geometry = new THREE.PlaneGeometry(10, 10, 64, 64);
 console.log(geometry);
 const material = new THREE.MeshStandardMaterial();
+material.side = THREE.DoubleSide;
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+mesh.rotation.x = 1.57;
+mesh.position.y = 1;
 
 // [3] Camera
 const aspect = {
